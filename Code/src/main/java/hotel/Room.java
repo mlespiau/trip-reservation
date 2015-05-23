2886fbf5a4d691cec81a663f02274ce96946310a
Matias Lespiau <mlespiau@avature.net>
2015-05-23 15:15:10 -0300

    Room does no longer needs to knows about TimeSlot

--- Code/src/main/java/hotel/Room.java
+++ Code/src/main/java/hotel/Room.java
@@ -1,36 +1,21 @@
 package hotel;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collection;
-
 import spark.QueryParamsMap;
-
-import com.google.gson.Gson;
 
 public class Room {
     private int code;
     private Hotel hotel;
     private int adultSpace;
     private int childrenSpace;
-    private Collection<TimeSlot> availability;
     
-    public Room(int code, Hotel hotel, int adultSpace, int childrenSpace, Collection<TimeSlot> availability) {
+    public Room(int code, Hotel hotel, int adultSpace, int childrenSpace) {
         this.code = code;
         this.hotel = hotel;
         this.adultSpace = adultSpace;
         this.childrenSpace = childrenSpace;
-        this.availability = availability;
     }
     
     public static Room fromQueryParams(QueryParamsMap queryMap) {
-        TimeSlot time = TimeSlot.create(
-            LocalDate.parse(queryMap.get("availableFrom").value(), DateTimeFormatter.ISO_LOCAL_DATE),
-            LocalDate.parse(queryMap.get("availableTo").value(), DateTimeFormatter.ISO_LOCAL_DATE)
-        );
-        Collection<TimeSlot> availability = new ArrayList<TimeSlot>();
-        availability.add(time);
         return new Room(
             queryMap.get("code").integerValue(),
             new Hotel(
@@ -40,8 +25,7 @@ public class Room {
                 queryMap.get("includesBreakfast").booleanValue()
             ),
             queryMap.get("adultSpace").integerValue(),
-            queryMap.get("childrenSpace").integerValue(),
-            availability
+            queryMap.get("childrenSpace").integerValue()
         );
     }
     
@@ -59,9 +43,5 @@ public class Room {
     
     public Hotel getHotel() {
         return hotel;
-    }
-    
-    public Collection<TimeSlot> getTimeAvailability() {
-        return availability;
     }
 }

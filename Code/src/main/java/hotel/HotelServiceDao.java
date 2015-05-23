133f14f752977345d9c2c384de057bd948617431
Matias Lespiau <mlespiau@avature.net>
2015-05-23 16:55:56 -0300

    Add TODOs

--- Code/src/main/java/hotel/HotelServiceDao.java
+++ Code/src/main/java/hotel/HotelServiceDao.java
@@ -1,5 +1,7 @@
 package hotel;
 
+// TODO: Verify the hotel does not exists
+// TODO: Verify the agentCode exists
 abstract public class HotelServiceDao {
     abstract public int save(Hotel hotel);
 }

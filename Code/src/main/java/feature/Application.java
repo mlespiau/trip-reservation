f636db465d23f43889fe28e6dcee667ec9523197
Matias Lespiau <mlespiau@avature.net>
2015-05-23 15:16:57 -0300

    Application: change endpoint to add new room time slots to /room/timeslot

--- Code/src/main/java/feature/Application.java
+++ Code/src/main/java/feature/Application.java
@@ -8,7 +8,7 @@ import hotel.TimeSlot;
 public class Application {
     public static void main(String[] args) {
         RoomTimeSlotService roomTimeSlotService = new RoomTimeSlotService();
-        post("/room", (req, res) -> roomTimeSlotService.create(
+        post("/room/timeslot", (req, res) -> roomTimeSlotService.create(
             Room.fromQueryParams(req.queryMap()),
             TimeSlot.fromQueryParams(req.queryMap())
         ), json());

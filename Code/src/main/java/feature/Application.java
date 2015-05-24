d5a29cb2935b570d37e23bed71ee3c2c92fd29b9
Matias Lespiau <mlespiau@avature.net>
2015-05-24 10:58:45 -0300

    Application: Extract class HotelAgentEndpoints

--- Code/src/main/java/feature/Application.java
+++ Code/src/main/java/feature/Application.java
@@ -1,41 +1,9 @@
 package feature;
-import static serialization.Json.json;
-import static spark.Spark.post;
-import hotel.Hotel;
-import hotel.HotelRepository;
-import hotel.HotelService;
-import hotel.Room;
-import hotel.RoomDao;
-import hotel.RoomService;
-import hotel.RoomTimeSlotService;
-import hotel.TimeSlot;
 
 public class Application {
     // TODO: agentCode should be part of the authentication API and be accessible if the user is an hotel agent
     // TODO: same with customerCode and systemAdminCode
     public static void main(String[] args) {
-        HotelService hotelService = new HotelService();
-        RoomService roomService = new RoomService(new RoomDao());
-        RoomTimeSlotService roomTimeSlotService = new RoomTimeSlotService();
-        HotelRepository hotelRepository = new HotelRepository();
-        RoomRepository roomRepository = new RoomRepository();
-        post("/hotel", (req, res) -> hotelService.saveNew(Hotel.fromQueryParams(req.queryMap())), json());
-        // TODO: here we need some validations hotel should 
-        // Hotel exist
-        // AgentCode is same as Hotel's agentCode (it means it can edit that hotel)
-        post("/hotel/room", (req, res) -> roomService .saveNew(
-           Room.fromQueryParams(
-               req.queryMap(),
-               hotelRepository.findByCode(Integer.parseInt(req.queryParams("hotelCode")))
-           )
-        ), json());
-        post("/hotel/room/timeslot", (req, res) -> roomTimeSlotService.create(
-            roomRepository.findByCode(
-                Integer.parseInt(req.queryParams("hotelCode")),
-                Integer.parseInt(req.queryParams("roomCode"))
-            ),
-            TimeSlot.fromQueryParams(req.queryMap())
-        ), json());
-        
+        new HotelAgentEndpoints();
     }
 }
---
+++ Code/src/main/java/feature/HotelAgentEndpoints.java
@@ -0,0 +1,39 @@
+package feature;
+
+import static serialization.Json.json;
+import static spark.Spark.post;
+import hotel.Hotel;
+import hotel.HotelRepository;
+import hotel.HotelService;
+import hotel.Room;
+import hotel.RoomDao;
+import hotel.RoomService;
+import hotel.RoomTimeSlotService;
+import hotel.TimeSlot;
+
+public class HotelAgentEndpoints {
+    public HotelAgentEndpoints() {
+        HotelService hotelService = new HotelService();
+        RoomService roomService = new RoomService(new RoomDao());
+        RoomTimeSlotService roomTimeSlotService = new RoomTimeSlotService();
+        HotelRepository hotelRepository = new HotelRepository();
+        RoomRepository roomRepository = new RoomRepository();
+        post("/hotel", (req, res) -> hotelService.saveNew(Hotel.fromQueryParams(req.queryMap())), json());
+        // TODO: here we need some validations hotel should 
+        // Hotel exist
+        // AgentCode is same as Hotel's agentCode (it means it can edit that hotel)
+        post("/hotel/room", (req, res) -> roomService .saveNew(
+           Room.fromQueryParams(
+               req.queryMap(),
+               hotelRepository.findByCode(Integer.parseInt(req.queryParams("hotelCode")))
+           )
+        ), json());
+        post("/hotel/room/timeslot", (req, res) -> roomTimeSlotService.create(
+            roomRepository.findByCode(
+                Integer.parseInt(req.queryParams("hotelCode")),
+                Integer.parseInt(req.queryParams("roomCode"))
+            ),
+            TimeSlot.fromQueryParams(req.queryMap())
+        ), json());
+    }
+}

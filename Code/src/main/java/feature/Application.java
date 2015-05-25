1dee44b6e99f138fbb191a9edc0134bd70d2b2d0
Matias Lespiau <mlespiau@avature.net>
2015-05-25 10:32:02 -0300

    Add todo

--- Code/src/main/java/feature/Application.java
+++ Code/src/main/java/feature/Application.java
@@ -14,6 +14,7 @@ import framework.RequestParameters;
 
 public class Application {
     // TODO: agentCode, customerCode and systemAdminCode should be part of the authentication API and be accessible from the user object
+    // TODO: hide some values/ids from json serialization
     public static void main(String[] args) {
         AuthorizationService authorizationService = new AuthorizationService();
         RequestParameters.registerExceptionHandler();
